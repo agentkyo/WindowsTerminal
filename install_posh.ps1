967e6f25eb910e21cd942ae7536d80a33765deee
agentkyo <caioviniciusxd@gmail.com>
2022-03-15 01:00:05 -0300

    v1

---
+++ install_posh.ps1
@@ -0,0 +1,6 @@
+Install-Module -Name oh-my-posh, posh-git, Terminal-Icons, Az.Tools.predictor
+Import-Module oh-my-posh
+Import-Module -Name terminal-icons
+Update-Module -Name psreadline
+Import-Module -Name psreadline
+Set-PoshPrompt -Theme clean-detailed
